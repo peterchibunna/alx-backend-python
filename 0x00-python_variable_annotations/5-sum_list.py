a0e5e6ee8c3ea9791d1ec98e8224f5b1af43fcbc
Peter Chibunna <peterchibunna@gmail.com>
2023-04-27 15:51:47 +0100

    TAsk #5

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+Module #5
+"""
+
+
+def sum_list(input_list: [float]) -> float:
+    """
+    returns the floating point sum of items in a list
+    :param input_list:
+    :return:
+    """
+    return float(sum(input_list))
