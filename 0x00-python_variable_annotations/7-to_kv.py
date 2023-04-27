0f8d922e07fb2a3b6873b2fe7f0e95771f7d833b
Peter Chibunna <peterchibunna@gmail.com>
2023-04-27 16:01:38 +0100

    Task #7

---
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Module #7:
+"""
+import typing
+
+
+def to_kv(k: str, v: typing.Union[int, float]) -> typing.Tuple[str, float]:
+    """
+    returns a tuple of k and v
+    :param k:
+    :param v:
+    :return:
+    """
+    return k, v
