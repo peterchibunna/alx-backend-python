2cb95d0ea8b4cb82e2cc2ecb8ab491c0e4fbee95
Peter Chibunna <peterchibunna@gmail.com>
2023-04-27 15:43:05 +0100

    Task #3

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+Module 3
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    returns the string representation of a floating point number
+    :param n:
+    :return:
+    """
+    return n.__str__()
