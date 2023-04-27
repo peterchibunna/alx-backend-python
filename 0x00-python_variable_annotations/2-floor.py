7fedf9f396753d0fb0dfda04373b700b34fb4867
Peter Chibunna <peterchibunna@gmail.com>
2023-04-27 15:19:13 +0100

    Task #2

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+Module 2:
+"""
+import math
+
+
+def floor(n: float) -> float:
+    """
+    returns the floored number n
+    :param n:
+    :return: `n` as a 'floored' number
+    """
+    return math.floor(n)
