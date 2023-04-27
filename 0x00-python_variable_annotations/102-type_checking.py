d514ee8f952a245a5d95d574842076535ef930d5
Peter Chibunna <peterchibunna@gmail.com>
2023-04-27 17:08:54 +0100

    Task #11

---
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""
+Module #12:
+"""
+from typing import Tuple, List, Union
+
+
+def zoom_array(lst: List[int], factor: int = 2) -> List:
+    """
+    Use mypy to validate the following piece of code and apply any necessary
+    changes.
+    :param lst:
+    :param factor:
+    :return:
+    """
+    zoomed_in: List = [
+        item for item in lst
+        for i in range(factor)
+    ]
+    return zoomed_in
+
+
+array = [12, 72, 91]
+
+zoom_2x = zoom_array(array)
+
+zoom_3x = zoom_array(array, 3)
