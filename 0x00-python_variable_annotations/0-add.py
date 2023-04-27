0cd24fd866454a4983824f31e77357d25d3ef556
Peter Chibunna <peterchibunna@gmail.com>
2023-04-27 15:12:50 +0100

    Tasks 0, 1

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+Module 0: Basic annotations - add
+"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    An addition function with type annotation
+    :param a:
+    :param b:
+    :return: the sum
+    """
+    return a + b
---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+module 1
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    joins 2 strings
+    :param str1: first string
+    :param str2: second string
+    :return: the joined strings
+    """
+    return str1 + str2
