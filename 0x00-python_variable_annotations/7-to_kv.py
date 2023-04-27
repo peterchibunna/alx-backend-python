f6a30f63cdaa13c4ff508d0f59dc61dc9fcd01ff
Peter Chibunna <peterchibunna@gmail.com>
2023-04-27 16:03:39 +0100

    Task #7

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -7,9 +7,9 @@ import typing
 
 def to_kv(k: str, v: typing.Union[int, float]) -> typing.Tuple[str, float]:
     """
-    returns a tuple of k and v
+    returns a tuple of k and v^2
     :param k:
     :param v:
     :return:
     """
-    return k, v
+    return str(k), float(v ** 2)
