dd22fed6d03841440fe0e41f969bc1f815f1b76b
Peter Chibunna <peterchibunna@gmail.com>
2023-04-27 16:40:57 +0100

    Task #11

---
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""
+Module #11
+"""
+import typing
+
+T = typing.TypeVar('T')
+
+
+def safely_get_value(
+        dct: typing.Mapping, key: typing.Any,
+        default: typing.Union[T, None] = None) -> typing.Union[~T, None]:
+    """11. More involved type annotations
+    :param dct:
+    :param key:
+    :param default:
+    :return:
+    """
+    if key in dct:
+        return dct[key]
+    else:
+        return default
