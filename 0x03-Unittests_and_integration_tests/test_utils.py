45e223b2ecde1ffa96a6a75b3227be0eac522e24
Peter Chibunna <peterchibunna@gmail.com>
2023-05-22 20:58:00 +0100

    1. Parameterize a unit test

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -20,3 +20,13 @@ class TestAccessNestedMap(unittest.TestCase):
     def test_access_nested_map(self, nested_map, path, expected) -> None:
         """Implement the named method"""
         self.assertEqual(access_nested_map(nested_map, path), expected)
+
+    @parameterized.expand([
+        ({}, ("a",), KeyError),
+        ({"a": 1}, ("a", "b"), KeyError),
+    ])
+    def test_access_nested_map_exception(self, nested_map, path, expected):
+        """Implement this test
+        """
+        with self.assertRaises(expected_exception=expected):
+            access_nested_map(nested_map, path)
