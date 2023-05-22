8604c8f0e8c5b254254c4b8e319a85a697325e46
Peter Chibunna <peterchibunna@gmail.com>
2023-05-22 20:27:51 +0100

    start trying the concept

---
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""
+0. Parameterize a unit test
+"""
+import unittest
+from parameterized import parameterized
+from typing import Dict, Tuple, Union
+from utils import access_nested_map
+
+
+class TestAccessNestedMap(unittest.TestCase):
+    """The main testcase class
+    """
+
+    @parameterized.expand([
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"),),
+    ])
+    def test_access_nested_map(self, nested_map: Dict, path: Tuple[str],
+                               expected: Union[Dict, int]) -> None:
+        """Implement the named method
+        """
+        self.assertEqual(access_nested_map(nested_map, path), expected)
